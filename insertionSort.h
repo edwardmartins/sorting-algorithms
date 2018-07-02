42c3f8e1247bdff9ebc9372c1ec1643905958190
Edu Martinez <36489953+EdwardMartinz@users.noreply.github.com>
2018-07-03 01:59:25 +0200

    Update insertionSort.h

--- insertionSort.h
+++ insertionSort.h
@@ -8,16 +8,16 @@ void insertionSort(vector <int> &v, int n) {
 	int current, pos;
 
 	for (int i = 1; i < n; i++) {
-		current = v[i]; // saves the current element
-		pos = i; // position of the vector until the ordered part, pos not included
+		current = v[i]; 
+		pos = i; // limit of the ordered part, pos not included
 
-		// compare the current value with each element in the ordered part
+		// while the elements in the ordered part are higher than the current value we make space
 		while (pos > 0 && v[pos - 1] > current) {
-			// while the elements in the ordered part are higher than the current value we make space
+			
 			v[pos] = v[pos - 1]; 
 			pos--;
 		}
-		if (pos != i) // if pos!= i we have made space to put the current the value
+		if (pos != i) // if pos != i we have made space so we move the current value
 			v[pos] = current; 
 	}
 }
