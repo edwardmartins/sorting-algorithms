17e41a23fe3d98031e24f8fd30f1c7bbf4229a4a
Edu Martinez <36489953+EdwardMartinz@users.noreply.github.com>
2018-07-02 20:08:27 +0200

    Update bubbleSort.h

--- bubbleSort.h
+++ bubbleSort.h
@@ -9,9 +9,8 @@ void bubbleSort(vector <int> &v, int n) {
 	int i = 0;
 
 	while (i < n - 1 && swapped) { // keep going while we swap in the unordered part
-
 		swapped = false;
-
+		
 		for (int j = n - 1; j > i; j--) { // unordered part
 
 			if (v[j] < v[j - 1]) {
